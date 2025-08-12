98a09045ca93e8f23880b575716f30116ac54dab
Yousuf Shabibi <shabibiyousuf@gmail.com>
2025-08-12 15:44:44 +0530

    same

--- seasonal_comparison.py
+++ seasonal_comparison.py
@@ -222,7 +222,7 @@ def plot_seasonal_comparison(indoor_df, outdoor_df, location, pollutant):
         ticktext=[str(i) for i in range(24)],
         tickfont=dict(family="Arial", size=14, color="black")),
     yaxis=dict(
-        tickfont=dict(family="Arial Black", size=10, color="black")),
+        tickfont=dict(family="Arial ", size=14, color="black")),
     legend=dict(
         font=dict(family="Arial ", size=14, color="black"))
     
